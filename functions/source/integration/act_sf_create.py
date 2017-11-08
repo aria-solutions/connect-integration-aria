0127c0d559935c6adc51ab7ce4145d0a5ebe26aa
Alonso Suarez <alonchb@gmail.com>
2017-11-07 20:00:40 -0700

    fix imports

--- functions/source/integration/act_sf_create.py
+++ functions/source/integration/act_sf_create.py
@@ -20,7 +20,7 @@ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTIO
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
 THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-import logging
+import logging, os, json
 from datetime import datetime, timedelta
 
 from salesforce import Salesforce
